e7ca10ec277d32c15f1606430f5d635bed11fab0
42Mykola <mykola.harasymchuk@bcestudent.com>
2025-01-29 10:31:24 +0000

    Carmine Buttons
    (Divine) Alan intervention has saved me 5 years of work!😼😼😼

--- Data-Assignment/main.py
+++ Data-Assignment/main.py
@@ -3,12 +3,14 @@ import pandas as pd
 import matplotlib as plt
 from tkinter import *
 from tkinter import ttk
-import csv
 import numpy as np
 import customtkinter as ctk
 
 #making a main page
 currentScene ="Main"
+
+#Type is set as a default criteria for choosing pokemon
+pokeChoice="Type"
 
 #Changing search to Poke-type
 searchpokemon = "Type"
@@ -17,77 +19,86 @@ searchpokemon = "Type"
 fileread = pd.read_csv('pokemon_data.csv')
 
 #colours for tkinter
-Wall1 = "#595A4A"             # #D0CFCF - Timberwolf  #595A4A - Ebony  #F6F7EB - Ivory  #331832 - Dark Purple  #A30015 - Carmine
-Wall2 = "#D0CFCF"
-Button = "#A30015"
-Button2 = "#331832"
-SearchBar = "#F6F7EB"
+Ebony = "#595A4A"             # #D0CFCF - Timberwolf  #595A4A - Ebony  #F6F7EB - Ivory  #331832 - Dark Purple  #A30015 - Carmine
+Timberwolf = "#D0CFCF"
+Carmine = "#A30015"
+DarkPurple = "#331832"
+Ivory = "#F6F7EB"
 
 
 #The app itself and it's framing
-app = ctk.CTk(fg_color=Wall1)
+app = ctk.CTk(fg_color=Ebony)
 app.geometry("700x700")
 app.title("Pokedex's Bizarre Adventure")
 app.resizable(width=False, height=False)
 
 
+#Search Bar
+
+
+def remove_pokemon_():
+    for widget in frameButtons.winfo_children():
+        widget.destroy()
+
+
+def search_call():
+    query = searchBar.get().strip().lower()
+    filteredData=fileread[
+                fileread['Type 1'].str.lower().str.contains(query) |
+                fileread['Type 2'].str.lower().str.contains(query)
+                ]
+    filteredData = filteredData.drop_duplicates(subset=
+    ['Name', 'Type 1', 'Type 2'])
+    remove_pokemon_()
+    for index, row in filteredData.iterrows():
+        name = row["Name"]
+        type1 = row["Type 1"]
+        # row.get is used because some pokemon do not have a second type
+        # and python doesn't like it if you use row[] and nothing appears
+        type2 = row.get("Type 2", None)
+        hp = row["HP"]
+        atk = row["Attack"]
+        defense = row["Defense"]
+        spAtk = row["Sp. Atk"]
+        spDef = row["Sp. Def"]
+        spd = row["Speed"]
+        gen = row["Generation"]
+        leg = row["Legendary"]
+
+        button = ctk.CTkButton(frameButtons,
+                               corner_radius=20, text=name, width=300, text_color="white", fg_color= Carmine,
+                               command=lambda
+                                   name=name,
+                                   type1=type1,
+                                   type2=type2,
+                                   hp=hp,
+                                   atk=atk,
+                                   defense=defense,
+                                   spAtk=spAtk,
+                                   spDef=spDef,
+                                   spd=spd,
+                                   gen=gen,
+                                   leg=leg:
+                               on_button_click(
+                                   name, type1, type2,
+                                   hp, atk, defense,
+                                   spAtk, spDef, spd, gen, leg))
+        button.pack(pady=5)
+
 #Fream for a search button, it fills the whole of x axis.
-searchFrame = ctk.CTkFrame(app, fg_color=Wall2)
+searchFrame = ctk.CTkFrame(app, fg_color=Timberwolf)
 searchFrame.pack(fill="x")
 
 
 #Search bar itself
-searchBar = ctk.CTkEntry(searchFrame, width= 500, fg_color= SearchBar)
+searchBar = ctk.CTkEntry(searchFrame, width= 500, fg_color= Ivory)
 searchBar.pack(side= "left", padx= 5)
+searchButton = ctk.CTkButton(searchFrame, command=search_call, fg_color= Carmine, text="Search")
+searchButton.pack(side="right")
 
+frameButtons = ctk.CTkScrollableFrame(app)
+frameButtons.pack(expand =True, fill="both")
 
-#Buttons of types of pokemons are displayed here
-typeButtonFrame = ctk.CTkFrame(app, fg_color=Wall2)
-typeButtonFrame.pack(anchor= "nw", padx= 5, pady= 5)
-
-
-#Search Bar
-def search_call():
-    query = searchBar.get()  .
-
-
-
-
-
-#definitions for finding by types
-
-#Fire
-def FindFire():
-    print (fileread.loc[fileread['Type 1'] == "Fire"])
-#Grass
-def FindGrass():
-    print (fileread.loc[fileread['Type 1'] == "Grass"])
-#Water
-def FindWater():
-    print (fileread.loc[fileread['Type 1'] == "Water"])
-#Ice
-def FindIce():
-    print (fileread.loc[fileread['Type 1'] == "Ice"])
-#Bug
-def FindBug():
-    print (fileread.loc[fileread['Type 1'] == "Bug"])
-
-
-#buttons
-button = ctk.CTkButton(typeButtonFrame, text="Show Fire Type", fg_color=Button, command = FindFire)
-button.pack()
-
-button = ctk.CTkButton(typeButtonFrame, text="Show Grass Type", fg_color=Button, command = FindGrass)
-button.pack()
-
-button = ctk.CTkButton(typeButtonFrame, text="Show Water Type", fg_color=Button, command = FindWater)
-button.pack()
-
-button = ctk.CTkButton(typeButtonFrame, text="Show Ice Type", fg_color=Button, command = FindIce)
-button.pack()
-
-button = ctk.CTkButton(typeButtonFrame, text="Show Bug Type", fg_color=Button, command = FindBug)
-button.pack()
 
 #second button
 #button2 = ctk.CTkButton(app, text="The Button 2™", fg_color=Button2)
