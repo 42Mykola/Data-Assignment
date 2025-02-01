6e3fbb0f157c7991583efae483c59808d9f26ab5
42Mykola <mykola.harasymchuk@bcestudent.com>
2025-02-01 14:56:46 +0000

    Graph Update
    First version of the graph is ready to go!

--- Data-Assignment/main.py
+++ Data-Assignment/main.py
@@ -5,6 +5,7 @@ from tkinter import *
 from tkinter import ttk
 import numpy as np
 import customtkinter as ctk
+import matplotlib.pyplot as plt
 
 #making a main page
 currentScene ="Main"
@@ -33,14 +34,55 @@ app.title("Pokedex's Bizarre Adventure")
 app.resizable(width=False, height=False)
 
 
-
 #defining on button click option
 resultStat = 0
+
+
+#Defining graph generation
+def generate_type_graph():
+    #Counting the number of Pokemon for each type is happening here. Function "value_counts()" counts each
+    #instane of specific Type 1 in the corresponding row, and then uses ".add" to add values from the rows
+    #with type 2. "Fill_value=0" makes sure that empty spots are countes as 0 in the equation.
+    type_counts = fileread['Type 1'].value_counts().add(fileread['Type 2'].value_counts(), fill_value=0)
+
+    #Dictionary of colors is created to correspond to each type.
+    colors = {
+        'Bug': '#4B81F5', #Actually Water
+        'Fire': '#4F3425', #Actually Ground
+        'Grass': '#4F433D', #Actually Rock
+        'Electric': '#329118', #Actually Grass
+        'Psychic': '#AEB0B1', #Actually Steel
+        'Ice': '#2F0131', #Actually Dark
+        'Dragon': '#C2F4FF', #Actually Flying
+        'Dark': '#A49586', #Actually Normal
+        'Fairy': '#995FB1', #Actually Psychic
+        'Fighting': '#CE60F7', #Actually Bug
+        'Rock': '#99AEAD', #Actually Ghost
+        'Ghost': '#9DE742', #Actually Poison
+        'Normal': '#FFE100', #Actually Electric
+        'Poison': '#C22222', #Actually Dragon
+        'Water': '#9AFCFF', #Actually Ice
+        'Ground': '#D08844', #Actually Fighting
+        'Steel': '#F395DD', #Actually Fairy
+        'Flying': '#E15320', #Actually Fire
+    }
+
+    #Colours are assigned based on type to the bars.
+    bar_colors = [colors[type_name] for type_name in type_counts.index]
+
+    # Plot the data
+    plt.figure(figsize=(10, 6))
+    type_counts.sort_values(ascending=False).plot(kind='bar', color = bar_colors)
+    plt.title('Number of Pokémon per Type')
+    plt.xlabel('Type')
+    plt.ylabel('Number of Pokémon')
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.show()
+
 def on_button_click(name, type1, type2,
                     hp, atk, defense,
                    spAtk, spDef, spd, gen, leg):
-    resultStat = name, type1, type2, hp, atk, defense, spAtk, spDef, spd, gen, leg
-    print(resultStat)
 
     #Clears the details Frame widget for new stats to be set
     detailsFrame.delete("1.0", "end")
@@ -65,7 +107,7 @@ def remove_pokemon_():
     for widget in frameButtons.winfo_children():
         widget.destroy()
 
-#Search bar functions and allat
+#Search bar functions and creation of buttons that corresponds to the pokemon
 def search_call():
     query = searchBar.get().strip().lower()
     filteredData=fileread[
@@ -117,6 +159,28 @@ def search_call():
 searchFrame = ctk.CTkFrame(app, fg_color=Timberwolf)
 searchFrame.pack(fill="x")
 
+#Frame for Other Buttons
+functionFrame = ctk.CTkFrame(searchFrame)
+functionFrame.pack(fill="x")
+
+#Graph buttons
+graphButton = ctk.CTkButton(functionFrame,
+                            command=generate_type_graph,
+                            fg_color=Carmine,
+                            hover_color=DarkPurple,
+                            text="Graph")
+graphButton.pack(side = "left", expand = True, padx = 5, pady = 5)
+DiamondButton = ctk.CTkButton(functionFrame,
+                            fg_color=Carmine,
+                            hover_color=DarkPurple,
+                            text="Diamond Graph")
+DiamondButton.pack(side = "left", expand = True, padx = 5, pady = 5)
+randomPokemonButton = ctk.CTkButton(functionFrame,
+                            fg_color=Carmine,
+                            hover_color=DarkPurple,
+                            text="Random Pokemon")
+randomPokemonButton.pack(side = "left", expand = True, padx = 5, pady = 5)
+
 #Frame for displaying Pokemon stats.
 detailsFrame = ctk.CTkTextbox(app)
 pokemonDetails = detailsFrame.insert("0.0", "Pokemon Details")
@@ -131,6 +195,9 @@ searchButton = ctk.CTkButton(searchFrame,
                              hover_color= DarkPurple,)
 searchButton.pack(side="right")
 
+
+
+
 #Buttons of Pokemons that pop up when type is searched for.
 frameButtons = ctk.CTkScrollableFrame(app)
 frameButtons.pack(expand =True, fill="both", side="left")
@@ -140,5 +207,4 @@ frameButtons.pack(expand =True, fill="both", side="left")
 #button2 = ctk.CTkButton(app, text="The Button 2™", fg_color=Button2)
 #button2.place(x=70, y=70)
 app.mainloop()
-
 
